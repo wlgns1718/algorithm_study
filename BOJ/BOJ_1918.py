293190650f4dba08758977e5564dcd37d6ce8ab0
신지훈 <wlgns1718@naver.com>
2023-02-15 21:00:07 +0900

    20230215

---
+++ BOJ/BOJ_1918.py
@@ -0,0 +1,31 @@
+"""
+ title: 후위표기식
+ level : Gold2
+ tag: data_structures
+ date:20230215
+"""
+
+strn = list(input())
+stack=[]
+res=''
+for s in strn:
+    if s.isalpha():
+        res+=s
+    else:
+        if s == '(':
+            stack.append(s)
+        elif s == '*' or s == '/':
+            while stack and (stack[-1] == '*' or stack[-1] =='/'):
+                res += stack.pop()
+            stack.append(s)
+        elif s == '+' or s == '-':
+            while stack and stack[-1] != '(':
+                res+= stack.pop()
+            stack.append(s)
+        elif s == ')':
+            while stack and stack[-1] != '(':
+                res += stack.pop()
+            stack.pop()
+while stack :
+    res+=stack.pop()
+print(res)
