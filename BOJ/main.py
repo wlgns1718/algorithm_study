3f5b6c8dba595864c23f2744b4e54ca75c90ead5
신지훈 <wlgns1718@naver.com>
2023-02-27 15:42:13 +0900

    20230227

--- BOJ/main.py
+++ BOJ/main.py
@@ -1,34 +1,13 @@
-def sol(queue,answer,visit,b,cnt):
-    while queue:
-        idx = queue.popleft()
-        for i in range(N + 1):
-            if idx in answer[i] and i not in visit:
-                queue.append(i)
-                cnt += 1
-                if b in answer[i]:
-                    return cnt+1
-                break
-    return -1
 import sys
-from collections import deque
+
 input = sys.stdin.readline
-N = int(input())
-answer = [[] for _ in range(N+1)]
 
 a,b = map(int,input().split())
-M = int(input())
-dict = {i : [] for i in range(1,N+1)}
-for _ in range(M):
-    x,y = map(int,input().split())
-    answer[x].append(y)
-print(answer)
-queue = deque();
-visit = set()
-cnt = 1
-for i in range(N+1):
-    if a in answer[i]:
-        queue.append(i)
-        visit.add(i)
-        break
+cnt = 0
 
-print(sol(queue,answer,visit,b,cnt))
+string  = str(a*b)
+for i in string[::-1]:
+    if i =='0':
+        cnt += 1
+    else:
+        break
